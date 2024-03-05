05eac7864c5d7047a61e65ba5d8854521391085f
OsmarMazo <osmar.mazo@correounivalle.edu.co>
2024-03-04 21:04:31 -0500

    primera subida juego

---
+++ PalabraDia/src/app/pages/jugar/celda/celda.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-celda',
+  templateUrl: './celda.component.html',
+  styleUrls: ['./celda.component.scss'],
+})
+export class CeldaComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ PalabraDia/src/app/pages/jugar/fila/fila.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fila',
+  templateUrl: './fila.component.html',
+  styleUrls: ['./fila.component.scss'],
+})
+export class FilaComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
